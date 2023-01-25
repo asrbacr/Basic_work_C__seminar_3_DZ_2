c5077e9c8617c96246bd9e8a405268f25e0877a1
Vasiliy Gorilev <89159793029@ya.ru>
2023-01-25 12:54:37 +0300

    add task 19

--- Program.cs
+++ Program.cs
@@ -10,28 +10,61 @@
 
 // Формула к 23
 
-Console.Write("Введите номер задачи для проверки: ");
+// Console.Write("Введите номер задачи для проверки: ");
 
-int UserInput(int number)
+// int UserInput(int number)
+// {
+//     while (!int.TryParse(Console.ReadLine(), out number))
+//         Console.Write("Вы ввели не число. Повторите попытку ввода: ");
+//     return number;
+// }
+// int b;
+// // int a = UserInput(b);
+
+// // swift(UserInput):
+// // {
+//     case 19:
+Console.Write("Введите пятизначное число для проверки является ли оно палиндромом: ");
+int a = UserInput();
+
+if (a < 0)
+    a *= (-1);
+
+if (!(a > 9999 && a < 100000))
+    Console.WriteLine("Придётся выбрать задачу заново, т.к. число не пятизначное");
+else
 {
+    if (a % 10 == a / 10000 && (a / 1000) % 10 == (a % 100) / 10)
+        Console.WriteLine("да");
+    else
+        Console.WriteLine("Нет");
+}
+
+
+
+
+// bool Polindrom(int number)
+// {
+//     if (number % 10 == number / 10000 && (number / 1000) % 10 == (number % 100) / 10)
+//     {
+//         Console.WrireLine("да");
+//     }
+// }
+
+
+// // }
+
+
+// Console.Write("Введите номер задачи для проверки ДЗ: ");
+
+
+//_________Funcs
+
+int UserInput()
+{
+    int number;
     while (!int.TryParse(Console.ReadLine(), out number))
-        Console.Write("Вы ввели не число. Повторите попытку ввода: ");
+        Console.Write("Вы ввели не число. Повторите ввод: ");
+
     return number;
 }
-int b;
-// int a = UserInput(b);
-
-// swift(UserInput):
-// {
-//     case 19:
-bool Polindrom(int number)
-{
-    if (number % 10 == number / 10000 && (number / 1000) % 10 == (number % 100) / 10)
-    {
-        Console.WrireLine("да");
-    }
-}
-Console.Write("Введите пятизначное число для проверки является ли оно палиндромом: ");
-int a = UserInput(b);
-
-// }
