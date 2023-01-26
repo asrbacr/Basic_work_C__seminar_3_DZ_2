ebf4b5f4835c7a221ae61dd118fd76c810168e2a
Vasiliy Gorilev <89159793029@ya.ru>
2023-01-26 13:12:58 +0300

    add structur task 21

--- Program.cs
+++ Program.cs
@@ -24,38 +24,58 @@
 // // swift(UserInput):
 // // {
 //     case 19:
-Console.Write("Введите пятизначное число для проверки является ли оно палиндромом: ");
-int a = UserInput();
+// Console.Write("Введите пятизначное число для проверки является ли оно палиндромом: ");
+// int a = UserInput();
 
-Polindrom(a);
+// Polindrom(a);
 
-void Polindrom(int a)
+// void Polindrom(int a)
+// {
+//     if (a < 0)
+//         a *= (-1);
+
+//     if (!(a > 9999 && a < 100000))
+//     {
+//         Console.WriteLine("Придётся выбрать задачу заново, т.к. число не пятизначное");
+//     }
+//     else
+//     {
+//         if (a % 10 == a / 10000 && (a / 1000) % 10 == (a % 100) / 10)
+//         {
+//             {
+//                 Console.WriteLine("да");
+//             }
+//         }
+//         else
+//         {
+//             Console.WriteLine("Нет");
+//         }
+//     }
+// }
+
+
+// case 21:
+
+int x1, y1, z1, x2, y2, z2;
+Console.Write("Введите значение X для 1 точки: ");
+x1 = UserInput();
+Console.Write("Введите значение Y для 1 точки: ");
+y1 = UserInput();
+Console.Write("Введите значение Z для 1 точки: ");
+z1 = UserInput();
+Console.Write("Введите значение X для 2 точки: ");
+x2 = UserInput();
+Console.Write("Введите значение Y для 2 точки: ");
+y2 = UserInput();
+Console.Write("Введите значение Z для 2 точки: ");
+z2 = UserInput();
+
+Console.WriteLine($"Расстояние между точками А ({x1},{y1},{z1}); B ({x2},{y2},{z2}) -> {Distance.ToString("0.##")}");
+
+double Distance(double x1, double y1, double z1, double x2, double y2, double z2)
 {
-    if (a < 0)
-        a *= (-1);
-
-    if (!(a > 9999 && a < 100000))
-    {
-        Console.WriteLine("Придётся выбрать задачу заново, т.к. число не пятизначное");
-    }
-    else
-    {
-        if (a % 10 == a / 10000 && (a / 1000) % 10 == (a % 100) / 10)
-        {
-            {
-                Console.WriteLine("да");
-            }
-        }
-        else
-        {
-            Console.WriteLine("Нет");
-        }
-    }
+    return Math.Sqrt((x2 - x1) + (y2 - y1) + (z2 - z1));
 }
-
-
-
-
 
 
 // bool Polindrom(int number)
